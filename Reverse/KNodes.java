2846352be6746a7a84c4b68a4e5e2a69038ac8fe
Saena <toastyhatesthesea@gmail.com>
2018-09-26 04:00:49 -0700

    Added Code to reverseKGroup and added a reverse function helper

--- Reverse/KNodes.java
+++ Reverse/KNodes.java
@@ -10,29 +10,47 @@ public class KNodes
     {
         ListNode answer = head;
         ListNode current = head;
+        ListNode previous = head;
+        ListNode end, beginning;
 
         while (current != null)
         {
+            beginning = current;
 
+            for(int i=0; i<k && current != null; i++)
+            {
+                end = current.next;
+            }
 
         }
-
 
 
         return answer;
     }
 
-
-    class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode(int x)
+    public void reverse(int amount, ListNode beg, ListNode end)
+    {
+        if (amount == 1)
         {
-            val = x;
+            end.next = beg;
+        }
+        else
+        {
+
         }
     }
 
+
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x)
+    {
+        val = x;
+    }
 }
 
 class KNodesTesters
@@ -42,6 +60,12 @@ class KNodesTesters
     public static void main(String[] agsg)
     {
         SwapNode blahs = new SwapNode();
+
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(3);
+        l1.next.next.next = new ListNode(4);
+        l1.next.next.next.next = new ListNode(5);
 
     }
 }
