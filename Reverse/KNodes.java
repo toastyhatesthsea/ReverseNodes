75249ca81ccc061deaed77050e5cac46810c4bd5
Saena <toastyhatesthesea@gmail.com>
2018-09-26 01:25:07 -0700

    Created initial reverseKGroup Function

---
+++ Reverse/KNodes.java
@@ -0,0 +1,47 @@
+package Reverse;
+
+import Swaps.SwapNode;
+
+
+public class KNodes
+{
+
+    public ListNode reverseKGroup(ListNode head, int k)
+    {
+        ListNode answer = head;
+        ListNode current = head;
+
+        while (current != null)
+        {
+
+
+        }
+
+
+
+        return answer;
+    }
+
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x)
+        {
+            val = x;
+        }
+    }
+
+}
+
+class KNodesTesters
+{
+
+
+    public static void main(String[] agsg)
+    {
+        SwapNode blahs = new SwapNode();
+
+    }
+}
