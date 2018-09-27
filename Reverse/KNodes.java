ad017f1dae976bcca2be5c611bba21dc5dbce35c
Saena <toastyhatesthesea@gmail.com>
2018-09-27 04:40:27 -0700

    Added reverseKGroup function comments

--- Reverse/KNodes.java
+++ Reverse/KNodes.java
@@ -4,6 +4,19 @@ package Reverse;
 public class KNodes
 {
 
+    /**Given a linked list, reverse the nodes of a linked list k at a time and return its modified list. Only uses constant O(1) memory references.
+
+     k is a positive integer and is less than or equal to the length of the linked list.
+     If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+     Example:
+     Given this linked list: 1->2->3->4->5
+     For k = 2, you should return: 2->1->4->3->5
+     For k = 3, you should return: 3->2->1->4->5
+     *
+     * @param head
+     * @param k
+     * @return
+     */
     public ListNode reverseKGroup(ListNode head, int k)
     {
         ListNode end = head , beginning = head, answer = head, current = head, previous = head;
